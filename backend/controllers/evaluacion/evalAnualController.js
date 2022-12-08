880f24b4f8dff98ba8d98c9de2743bdd548070c7
Francis <francisbg.123@gmail.com>
2022-12-07 21:09:09 -0400

    commit francis 2

---
+++ backend/controllers/evaluacion/evalAnualController.js
@@ -0,0 +1,8 @@
+import insertQuery from "../utils/insertQuery.js"
+import selectQuery from "../utils/selectQuery.js"
+
+const productorasContratoPost = (req, res) => {
+    const {id_client} = req.body
+
+    
+}
