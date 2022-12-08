63cd762d14aa2df4473cb526442ea0c33544b2b3
Francis <francisbg.123@gmail.com>
2022-12-07 20:59:41 -0400

    commit 2 Francis - creacion de todo el ambiente

---
+++ backend/routers/empresasRouters/clienteRouter.js
@@ -0,0 +1,10 @@
+import { Router } from "express"
+import { clienteController } from "../../controllers/empresasController/cliente.js"
+
+const router = Router()
+
+router.get('', clienteController.empresasClienteGet)
+router.get('/registro', clienteController.ciudadesGet)
+router.post('/registro', clienteController.empresasClientePost)
+
+export const clienteRouter = router
