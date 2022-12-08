74e7af6e15c97ceb1c312cf247c79c60c0097835
Francis <francisbg.123@gmail.com>
2022-12-08 07:43:02 -0400

    francis commit 2

--- backend/controllers/evaluacion/evalAnualController.js
+++ backend/controllers/evaluacion/evalAnualController.js
@@ -4,5 +4,12 @@ import selectQuery from "../utils/selectQuery.js"
 const productorasContratoPost = (req, res) => {
     const {id_client} = req.body
 
-    
+    const condicion = " WHERE c.id_client = " + id_client + " AND c.estatus = 'Activo' AND c.id_prod = p.id"
+
+    selectQuery("p.id id_prod, p.nombre nombre, c.id contrato", " fah_empresasproductoras p, fah_contratos c ", condicion, '', (err, result) => {
+        if (err)
+            res.status(500).send(err)
+        else
+            res.json(result)
+    })
 }
